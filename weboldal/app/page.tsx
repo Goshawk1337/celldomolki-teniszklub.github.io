7ac0869c04f4d804c731f30a7ef79c3c0bfec3c8
Goshawk1337 <77876216+Goshawk1337@users.noreply.github.com>
2026-06-02 22:48:19 +0200

    Minor fixes, and the courts section is done.

--- weboldal/app/page.tsx
+++ weboldal/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/header";
 import Hero from "@/components/hero";
-import About from "@/components/ui/about";
+import About from "@/components/about";
+import Courts from "@/components/courts";
 export default function Home() {
   return (
      <main className="min-h-screen">
        <Header />
        <Hero />
        <About />  
+       <Courts />
      </main>
   );
 }
--- weboldal/components/about.tsx
+++ weboldal/components/about.tsx
@@ -1,5 +1,5 @@
 import { Plus } from "lucide-react";
-import { Button } from "./button";
+import { Button } from "./ui/button";
 const features = [
     {
         title: "Személyre szabott edzés",
